b024cb3a58ab06cb0809fa585564ff565b077773
Andre <kappaj@gmail.com>
2017-11-20 16:02:48 +0200

    Woring in Google PubSub

--- src/main/java/za/co/ajk/incidentman/web/rest/TestSending.java
+++ src/main/java/za/co/ajk/incidentman/web/rest/TestSending.java
@@ -5,12 +5,14 @@ import java.util.stream.IntStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import za.co.ajk.incidentman.messaging.MessageSender;
+//import za.co.ajk.incidentman.messaging.googlepubsub.GoogleChannelManager;
+
 
 @RestController
 public class TestSending {
@@ -19,9 +21,18 @@ public class TestSending {
     @Autowired
     private MessageSender messageSender;
     
+//    @Autowired
+//    private GoogleChannelManager.PubsubOutboundGateway messagingGateway;
+//
+    
     @RequestMapping("/send/{nr}")
     public void submitMessage(@PathVariable("nr") Integer nr){
         log.info("Sending "+nr+"  message(s)!!");
         IntStream.range(0, nr).forEach(ii -> messageSender.sendMessage(ii));
+    }
+    
+    @GetMapping("/testGoogle/submit/{ii}")
+    public void testSendingToGooglePubSub(final @PathVariable String ii){
+        //messagingGateway.sendToPubsub("Testing pub sub "+ii);
     }
 }
